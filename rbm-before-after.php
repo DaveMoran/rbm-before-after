03d95d3ae8cef176e31e1b1bff133d9b2ce7ed0b
Dave Moran <dave.moran118@gmail.com>
2016-12-06 17:45:36 -0500

    Change version number of plugin

--- rbm-before-after.php
+++ rbm-before-after.php
@@ -3,7 +3,7 @@
  Plugin Name: RBM Before After
  Plugin URI: http://reachbeyondmarketing.com
  Description: A plugin to display before and after photos in an easier manner
- Version: 0.0
+ Version: 0.1
  Author: Dave M
  Author URI: https://dmoran.co
  License: GPL2
