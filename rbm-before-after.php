615194abdfc50561ecb77a06992d0baf2f80edb9
Dave Moran <dave.moran118@gmail.com>
2016-12-06 17:27:12 -0500

    Initial Commit

---
+++ rbm-before-after.php
@@ -0,0 +1,10 @@
+<?php
+/*
+ Plugin Name: RBM Before After
+ Plugin URI: http://reachbeyondmarketing.com
+ Description: A plugin to display before and after photos in an easier manner
+ Version: 0.0
+ Author: Dave M
+ Author URI: https://dmoran.co
+ License: GPL2
+ */
