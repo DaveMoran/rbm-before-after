e789d4b740feed5d1204ff9184348827f90adb34
Dave Moran <dave.moran118@gmail.com>
2016-12-07 10:53:03 -0500

    setup foreach loop for photos

--- shortcode.php
+++ shortcode.php
@@ -3,14 +3,22 @@
 //Create a test shortcode
 
 function rbm_gallery_shortcode($atts) {
+  //Step 1, setup basic shortcode arguements
   $args = shortcode_atts( array(
     'photos'  =>  '710',
     'title'   =>  'Case #'
   ), $atts);
 
+  //Step 2, save the new arguement vallues into variables
+  $photos = explode(',', $args['photos']);
+  $title = $args['title'];
+
   ob_start(); ?>
     <div class="gallery-wrapper">
-      <p>Photos: <?php echo $args['photos']; ?>, Title: <?php echo $args['title']; ?></p>
+      <?php foreach($photos as $photo){ ?>
+          <a href="#" class="gallery-item"><?php echo $photo; ?></a>
+       <?php } ?>
+      <p>Photos: <?php print_r($photos); ?>, Title: <?php echo $title; ?></p>
     </div> <?php
     $output = ob_get_clean();
   return $output;
