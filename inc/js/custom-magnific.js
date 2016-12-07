ec21151982b66041d83ca14e08d7917205e52482
Dave Moran <dave.moran118@gmail.com>
2016-12-07 11:12:42 -0500

    Edit script to seperate galleries

--- inc/js/custom-magnific.js
+++ inc/js/custom-magnific.js
@@ -1,18 +1,20 @@
 jQuery(document).ready(function($){
-  $('.gallery-wrapper').magnificPopup({
-    delegate: 'a', // child items selector, by clicking on it popup will open
-    type: 'image',
-    gallery: {
-      enabled: true
-    },
-    image: {
-      titleSrc: function(item) {
-        if(item.el.children('img').attr('alt').length > 0) {
-          return item.el.children('img').attr('alt');
-        } else {
-          return "";
+  $('.gallery-wrapper').each(function() {
+    $(this).magnificPopup({
+      delegate: 'a', // child items selector, by clicking on it popup will open
+      type: 'image',
+      gallery: {
+        enabled: true
+      },
+      image: {
+        titleSrc: function(item) {
+          if(item.el.children('img').attr('alt').length > 0) {
+            return item.el.children('img').attr('alt');
+          } else {
+            return "";
+          }
         }
       }
-    }
+    });
   });
 });
