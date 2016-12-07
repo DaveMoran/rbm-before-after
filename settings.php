8e3c2e41a9789979f4e41b3da1e6d4b06ad34333
Dave Moran <dave.moran118@gmail.com>
2016-12-07 12:19:16 -0500

    Add wrapper around enqueue to check for settings + label update

--- settings.php
+++ settings.php
@@ -17,13 +17,19 @@ function rbm_plugin_settings(){
 	//Step 3: Introduce the field to toggle enqueueing the scripts
 	add_settings_field(
 		'magnific_scripts',
-		'Magnific Scripts & Styles',
+		'Magnific Popup Scripts',
 		'magnific_toggle_callback',
 		'general',
 		'scripts_settings_section',
 		array(
 			'Activate this setting to enqueue Magnific Popup'
 		)
+	);
+
+	// Step 5, register the setting
+	register_setting(
+		'general',
+		'magnific_scripts'
 	);
 }
 
@@ -35,7 +41,7 @@ function scripts_settings_callback() {
 
 // Step 4: Create callback for custom field
 function magnific_toggle_callback( $args ) {
-	$html = '<input type="checkbox" id="magnific_scripts" name="enqueue_magnificmagnific_scripts" value="1" ' . checked(1, get_option('magnific_scripts'), false) . '/>';
+	$html = '<input type="checkbox" id="magnific_scripts" name="magnific_scripts" value="1" ' . checked(1, get_option('magnific_scripts'), false) . '/>';
 	$html .= '<label for="magnific_scripts">' . $args[0] . '</label>';
 	echo $html;
 }
